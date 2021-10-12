91356baf49f2780ce13026271f6fc9d1876da0b2
k0laedr <andre.oleynik@gmail.com>
2021-10-12 08:56:24 +0300

    add deploy

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ cd dist
 # если вы публикуете на пользовательский домен
 # echo 'www.example.com' > CNAME
 
-git init
+# git init
 git add -A
 git commit -m 'deploy'
 
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:academweb/aspire.git master:gh-pages
 
 cd -
