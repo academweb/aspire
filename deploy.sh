99b4675bc61c901499608c9a47ecd9e80a3b347f
k0laedr <andre.oleynik@gmail.com>
2021-10-12 09:03:25 +0300

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:academweb/aspire.git master:gh-pages
+git subtree push --prefix dist -f git@github.com:academweb/aspire.git master:gh-pages
 
 cd -
