55b31b719001cd9016f64f14c6901b71674cffc7
k0laedr <andre.oleynik@gmail.com>
2021-10-18 15:09:06 +0300

    add computed form and add action btn

--- src/components/CustomerForm.vue
+++ src/components/CustomerForm.vue
@@ -87,14 +87,13 @@
       </div>
       <div class="mb-4 fs-5">
         <p>Choose the number of each type of call you'd like to purchase</p>
-
         <div class="row align-items-center mb-4">
-          <div class="col-12 col-md-2 mb-0">Custom</div>
+          <div class="col-12 col-md-2 mb-0">Standart</div>
           <div class="col-6 col-md-2">
             <select
               class="form-select"
-              aria-label="Custom"
-              v-model="monthly.custom"
+              aria-label="Standart"
+              v-model="monthly.standart"
             >
               <option selected value="null" disabled>Select</option>
               <option v-for="m in 999" :value="m" :key="m">{{ m }}</option>
@@ -104,12 +103,12 @@
         </div>
 
         <div class="row align-items-center mb-4">
-          <div class="col-12 col-md-2 mb-0">Standart</div>
+          <div class="col-12 col-md-2 mb-0">Custom</div>
           <div class="col-6 col-md-2">
             <select
               class="form-select"
-              aria-label="Standart"
-              v-model="monthly.standart"
+              aria-label="Custom"
+              v-model="monthly.custom"
             >
               <option selected value="null" disabled>Select</option>
               <option v-for="m in 999" :value="m" :key="m">{{ m }}</option>
@@ -180,7 +179,9 @@
                 id="all_year"
                 v-model="exclude"
               />
-              <label class="form-check-label" for="all_year"> All year </label>
+              <label class="form-check-label" for="all_year"
+                >No exclusions (run calls all year)
+              </label>
             </div>
           </div>
           <div class="col-12 col-md-6">
@@ -240,9 +241,12 @@
         <ul class="list-group">
           <CallTypeForm
             :data="call"
-            v-for="call in calls"
-            :key="call.type"
+            v-for="(call, index) in calls"
+            :key="index"
             @addCall="addCall"
+            @removeCall="removeCall"
+            :new-item="calls.length && calls.length == index + 1"
+            :index="index"
           />
         </ul>
       </div>
@@ -250,70 +254,109 @@
     <FormBlock header="Notifications">
       <div class="">
         <p>Who should receive shop call notifications?</p>
-        <p>Enter email addresses, separated by commas</p>
       </div>
       <div
         class="row align-items-center mb-2"
         v-for="(email, index) in notification_emails"
         :key="index"
       >
-        <div class="col-10">
+        <div class="col-10 col-md-4">
           <input
             class="form-control"
-            type="text"
-            placeholder="Emails"
+            type="email"
+            placeholder="Email"
             aria-label=".form-control-lg example"
             v-model="notification_emails[index]"
+            required
           />
         </div>
         <div class="col-2">
-          <svg
-            @click="addEmail"
-            xmlns="http://www.w3.org/2000/svg"
-            width="26"
-            height="26"
-            fill="currentColor"
-            class="bi bi-journal-plus"
-            viewBox="0 0 16 16"
+          <button
+            class="btn"
+            v-if="
+              notification_emails.length &&
+              notification_emails.length == index + 1
+            "
+            @click.prevent="addEmail"
           >
-            <path
-              fill-rule="evenodd"
-              d="M8 5.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 .5-.5z"
-            />
-            <path
-              d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"
-            />
-            <path
-              d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"
-            />
-          </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="26"
+              height="26"
+              fill="currentColor"
+              class="bi bi-journal-plus"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 5.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 .5-.5z"
+              />
+              <path
+                d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"
+              />
+              <path
+                d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"
+              />
+            </svg>
+          </button>
+          <button v-else @click.prevent="removeEmail(index)" class="btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="26"
+              height="26"
+              fill="currentColor"
+              class="bi bi-journal-minus text-danger"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z"
+              />
+              <path
+                d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"
+              />
+              <path
+                d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"
+              />
+            </svg>
+          </button>
         </div>
       </div>
     </FormBlock>
+
     <FormBlock header="Order Summary">
-      <table class="table table-borderless fs-5">
+      <table class="table table-borderless table-responsive">
         <tbody>
           <tr>
-            <td>5 Custom/mo.</td>
-            <td>60/year</td>
-            <td>$ x.xxx.xx</td>
+            <td>{{ monthly.standart }} Standart/mo.</td>
+            <td>{{starting_date}}</td>
+            <td>{{ resultTableData.standartAmoutn }}</td>
           </tr>
           <tr>
-            <td>5 Standart/mo.</td>
-            <td>60/year</td>
-            <td>$ x.xxx.xx</td>
+            <td>{{ monthly.custom }} Custom/mo.</td>
+            <td>{{resultTableData.calls.length ? '--' : starting_date}}</td>
+            <td>{{ resultTableData.customAmoutn }}</td>
+          </tr>
+          <tr v-for="(coll, index) in resultTableData.calls" :key="coll.id" class="text-muted" style="font-size: 14px">
+            <td class="ps-4">{{index+1}}. {{ coll.label }}</td>
+            <td>{{coll.start}} </td>
+            <td>{{ coll.amount }}<br class="d-block d-md-none"> ({{coll.count}} x {{ coll.price }})</td>
           </tr>
           <tr>
-            <td colspan="2">Annual discount (applied)</td>
-            <td>$ x.xxx.xx</td>
+            <td colspan="2">{{ discountArray[discount].label }} (applied)</td>
+            <td>{{ resultTableData.discount }}</td>
+          </tr>
+          <tr>
+            <td colspan="2">Payment fee</td>
+            <td>{{ resultTableData.payment_fee }}</td>
           </tr>
           <tr>
             <td colspan="2"></td>
             <td><hr class="my-0" /></td>
           </tr>
-          <tr>
-            <td colspan="2" class="fw-bold">Total</td>
-            <td>$ x.xxx.xx</td>
+          <tr class="fw-bold fs-3">
+            <td colspan="2">Total</td>
+            <td>{{ resultTableData.total }}</td>
           </tr>
         </tbody>
       </table>
@@ -327,6 +370,15 @@ import FormBlock from "./components/FormBlock.vue";
 import CallTypeForm from "./components/CallTypeForm.vue";
 import mock from "./utils/mock";
 import Tooltip from "./components/Tooltip.vue";
+
+const moneyFilter = (value) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    maximumFractionDigits: 2,
+  }).format(value);
+};
+
 export default {
   components: {
     FormBlock,
@@ -352,11 +404,21 @@ export default {
         "Nov",
         "Dec",
       ],
+      discountArray: {
+        quarterly: {
+          label: "Quarterly discount 5%",
+          amount: 5,
+        },
+        annual: {
+          label: "Annual discount 10%",
+          amount: 10,
+        },
+      },
       client: null,
       property: null,
       monthly: {
-        custom: null,
-        standart: null,
+        custom: 1,
+        standart: 1,
       },
       discount: "quarterly",
       starting_date: null,
@@ -374,6 +436,46 @@ export default {
       ],
       notification_emails: [""],
     };
+  },
+  computed: {
+    resultTableData() {
+      const standartPrice =
+        this.callTypes.find((call) => call.id === 2)?.price || 1;
+      const standartCallsAmount = standartPrice * this.monthly.standart;
+      const customCallsAmount =
+        this.calls.reduce((acc, c) => {
+          const price =
+            this.callTypes.find((call) => call.id === c.type)?.price || 1;
+          return c?.type ? acc + c.count * price : acc;
+        }, 0) * this.monthly.custom;
+      const total = customCallsAmount + standartCallsAmount;
+      const discount =
+        total -
+        total * ((100 - this.discountArray[this.discount].amount) / 100);
+      const payment_fee =
+        this.discount === "annual" ? "3.5%" : "3.5% (per payment)";
+      const notEmpty = val => !!val.type && !!val.count
+      const calls = this.calls.filter(notEmpty).map((c) => {
+        const callItem = this.callTypes.find((call) => !c?.type || call.id === c.type);
+        return {
+          label: callItem?.name,
+          count: c.count,
+          start: c.start_month,
+          price: moneyFilter(callItem?.price),
+          amount: moneyFilter(callItem?.price * c.count),
+        };
+      });
+      return {
+        custom: null,
+        standart: null,
+        standartAmoutn: moneyFilter(standartCallsAmount),
+        customAmoutn: moneyFilter(customCallsAmount),
+        total: moneyFilter(total - discount),
+        discount: moneyFilter(discount),
+        payment_fee: payment_fee,
+        calls: calls,
+      };
+    },
   },
   methods: {
     async getCallTypes() {
@@ -406,9 +508,14 @@ export default {
         start_month: null,
       });
     },
-    addEmail(e) {
-      console.log("e :>> ", e);
+    addEmail() {
       this.notification_emails.push("");
+    },
+    removeEmail(index) {
+      this.notification_emails.splice(index, 1);
+    },
+    removeCall(index) {
+      this.calls.splice(index, 1);
     },
   },
   async mounted() {
--- src/components/components/CallTypeForm.vue
+++ src/components/components/CallTypeForm.vue
@@ -1,8 +1,8 @@
 <template>
-  <li class="list-group-item d-flex justify-content-between align-items-center">
-    <div class="">
-      <div class="row align-items-center">
-        <div class="col-12 col-md-6">
+  <li class="list-group-item">
+    <div class="d-flex flex-wrap justify-content-between align-items-center">
+      <div class="col-12 col-md-11 d-flex flex-wrap align-items-center">
+        <div class="col-12 col-md-6 px-0 px-md-2">
           <label for="call_type" class="form-label">Call type</label>
           <select
             id="call_type"
@@ -16,7 +16,7 @@
             </option>
           </select>
         </div>
-        <div class="col-12 col-md-3">
+        <div class="col-12 col-md-3 px-0 px-md-2">
           <label for="call_count" class="form-label"># of calls</label>
           <select
             id="call_count"
@@ -28,7 +28,7 @@
             <option v-for="m in 999" :value="m" :key="m">{{ m }}</option>
           </select>
         </div>
-        <div class="col-12 col-md-3">
+        <div class="col-12 col-md-3 px-0 px-md-2">
           <label for="call_count" class="form-label">Month *</label>
           <select
             class="form-select"
@@ -40,32 +40,72 @@
           </select>
         </div>
 
-        <span class="col-12 text-muted mt-1" style="font-size: 14px"
-          >* - select the monthly you'd like there calls to run</span
+        <span class="col-12 text-muted mt-2 text-end px-2 " style="font-size: 14px"
+          >* - select the month you'd like there calls to run</span
         >
       </div>
-    </div>
-    <div class="ms-3 row justify-content-end align-items-center" @click="$emit('addCall', true)">
-      <svg
-        
-        xmlns="http://www.w3.org/2000/svg"
-        width="50"
-        height="50"
-        fill="currentColor"
-        class="bi bi-clipboard-plus"
-        viewBox="0 0 16 16"
+
+      <div
+        class="
+          col-12 col-md-1
+          d-flex
+          flex-wrap
+          justify-content-end
+          align-items-center
+        "
       >
-        <path
-          fill-rule="evenodd"
-          d="M8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z"
-        />
-        <path
-          d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"
-        />
-        <path
-          d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"
-        />
-      </svg>
+        <button
+          class="btn d-flex align-items-center"
+          v-if="newItem"
+          @click.prevent="$emit('addCall', true)"
+        >
+          <div class="d-block d-md-none me-2 text-muted">
+            Add other call type
+          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="36"
+            height="36"
+            fill="currentColor"
+            class="bi bi-clipboard-plus"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z"
+            />
+            <path
+              d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"
+            />
+            <path
+              d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"
+            />
+          </svg>
+        </button>
+        <button v-else class="btn  d-flex align-items-center text-danger">
+          <div class="d-block d-md-none me-2 text-danger">Delete</div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="36"
+            height="36"
+            fill="currentColor"
+            class="bi bi-clipboard-x "
+            viewBox="0 0 16 16"
+            @click="$emit('removeCall', index)"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M6.146 7.146a.5.5 0 0 1 .708 0L8 8.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 9l1.147 1.146a.5.5 0 0 1-.708.708L8 9.707l-1.146 1.147a.5.5 0 0 1-.708-.708L7.293 9 6.146 7.854a.5.5 0 0 1 0-.708z"
+            />
+            <path
+              d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"
+            />
+            <path
+              d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   </li>
 </template>
@@ -82,6 +122,8 @@ export default {
         start_month: null,
       }),
     },
+    newItem: Boolean,
+    index: Number,
   },
   data() {
     return {
